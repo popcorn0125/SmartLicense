fbfeeb37813027829756fbc1aece007f6b002245
김준식 <105399710+popcorn0125@users.noreply.github.com>
2024-07-03 14:45:35 +0900

    240703 김준식 회원가입 페이지 구현

---
+++ src/components/userSignPage.vue
@@ -0,0 +1,258 @@
+<template>
+    <div id="screen">
+        <div class="header">
+            <img :src="imagePath" alt="no_image" />
+        </div>
+        <div class="body">
+            <div class="container">
+                <label for="id">아이디</label>
+                <div class="input-group">
+                    <input type="text" class="id" name="id" placeholder="아이디">
+                    <button class="id-Btn">중복확인</button>
+                </div>
+            </div>
+            <div class="container space">
+                <label for="pw">비밀번호</label>
+                <div class="input-group">
+                    <input type="password" class="pw" name="pw" placeholder="비밀번호">
+                </div>
+                <text>8자리 이상 영문, 대소문자 구분, 특수기호 포함</text>
+            </div>
+            <div class="container space">
+                <div class="pwCheck">
+                    <label for="pw">비밀번호 확인</label>
+                    <img :src="pwCheckImage" alt="이"/>
+                    <text class="pwCheckMsg">비밀번호가 일치하지 않습니다.</text>
+                </div>
+                <div class="input-group">
+                    <input type="password" class="pw" name="pw" placeholder="비밀번호 재입력">
+                </div>
+            </div>
+            <div class="container space">
+                <label for="name">이름</label>
+                <div class="input-group">
+                    <input type="text" class="pw" name="name" placeholder="이름(실명 입력)">
+                </div>
+            </div>
+            <div class="container space">
+                <label for="nickname">닉네임</label>
+                <div class="input-group">
+                    <input type="text" class="id" name="nickname" placeholder="닉네임(10자리)">
+                    <button class="id-Btn">중복확인</button>
+                </div>
+            </div>
+            <div class="container space">
+                <text class="gender-text">성별</text>
+                <div class="input-group">
+                    <button :class="{'selected': selectedGender === '남자', 'not-selected': selectedGender !== '남자'}" class="gender-Btn" @click="selectGender('남자')">남자</button>
+                    <button :class="{'selected': selectedGender === '여자', 'not-selected': selectedGender !== '여자'}" class="gender-Btn woman" @click="selectGender('여자')">여자</button>
+                </div>
+            </div>
+            <div class="container space">
+                <label for="phoneNum">전화번호</label>
+                <div class="input-group">
+                    <input type="text" class="id" name="phoneNum" placeholder="전화번호">
+                    <button class="id-Btn">인증번호 전송</button>
+                </div>
+            </div>
+            <div class="container space">
+                <label for="phoneNumCheck">인증번호</label>
+                <div class="input-group">
+                    <input type="text" class="id" name="phoneNumCheck" placeholder="인증번호">
+                    <button class="id-Btn">인증번호 확인</button>
+                </div>
+            </div>
+            <div class="container space">
+                <button class="black_long_btn">회원가입</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            imagePath: require('@/assets/gotoback.png'),
+            pwCheckImage: require('@/assets/pwcheck.png'),
+            selectedGender: "" // 성별 선택 여부
+        }
+    },
+
+    methods: {
+        selectGender(gender) {
+            this.selectedGender = gender;
+        }
+    },
+
+    created() {
+
+    },
+    mounted() {
+
+    },
+}
+</script>
+<style scoped>
+@import "@/css/common.css";
+#screen {
+  width: 100%;
+  height: 100vh; /* 화면 전체 높이 */
+  background-color: #5271FF;
+  /* text-align: center; */
+  /* align-items: center; 수직 중앙 정렬 */
+  /* justify-content: center; 수평 중앙 정렬 */
+}
+
+label {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 1em;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+.header {
+    display: block;
+    width: auto;
+    height: 5%;
+    justify-content: left;
+    align-items: center;
+    /* border: 1px solid black; */
+    margin: 14px 0 0 11px;
+}
+
+.body{
+    width: 100%;
+    height: calc(100% - 14px - 5%);
+    /* border: 1px solid black; */
+}
+
+.container {
+    display: block;
+    width: 80%;
+    height: auto;
+    margin: 0 auto;
+}
+
+.input-group {
+    display: flex;
+    width: 100%;
+    height: 29px;
+    margin-top: 5px;
+}
+
+.id {
+    display: flex;
+    width: 100%;
+    height: 29px;
+    padding: 0 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+    background-color: #fff;
+    font-size: 1em;
+    color: #333;
+    
+}
+
+.id-Btn{
+    width: auto;
+    height: 29px;
+    flex-shrink: 0;
+    border-radius: 5px;
+    background: #000;
+    color: #fff;
+    display: flex;
+    padding: 0 10px;
+    align-items: center;
+    font-family: Inter;
+    font-size: 1em;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-left: 10px;
+}
+
+.space{
+    margin-top: 15px;
+
+}
+
+.pw {
+    display: flex;
+    width: 100%;
+    height: 29px;
+    padding: 0 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+    background-color: #fff;
+    font-size: 1em;
+    color: #333;
+}
+
+text {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 0.7em;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.pwCheckMsg {
+    color: #F00;
+    -webkit-text-stroke-width: 1;
+    -webkit-text-stroke-color: #000;
+    font-family: Inter;
+    font-size: 0.7em;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+/***** 성별 선택 ****/
+.gender-text {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 1em;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+.gender-Btn {
+    flex-shrink: 0;
+    width: 40%;
+    height: 34px;
+    border-radius: 5px;
+    background: #000;
+    color: #fff;
+    display: flex;
+    padding: 0 10px;
+    align-items: center;
+    font-family: Inter;
+    font-size: 0.9em;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    align-items: center;
+    justify-content: center; /* 가로 방향 가운데 정렬 */
+}
+.woman {
+    margin-left: auto;
+}
+.selected {
+    background-color: grey; /* 선택된 버튼의 배경색 */
+    color: white; /* 선택된 버튼의 글자색 */
+}
+
+.not-selected {
+    background-color: black; /* 선택되지 않은 버튼의 배경색 */
+    color: white; /* 선택되지 않은 버튼의 글자색 */
+}
+
+
+.black_long_btn {
+    width: 100%;
+    margin-top: 5%;
+}
+</style>
