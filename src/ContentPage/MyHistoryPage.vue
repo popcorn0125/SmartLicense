21a8b2f3000315ba4756782473ac16dbfb05bddc
gyk419 <gyk419@gmail.com>
2024-08-07 16:57:57 +0900

    240807 권지용 검색기능 구현

--- src/ContentPage/MyHistoryPage.vue
+++ src/ContentPage/MyHistoryPage.vue
@@ -8,7 +8,8 @@
                 </path>
             </g>
         </svg>
-        <input class="input" type="search" placeholder="세부자격을 검색하세요." />
+        <input ref="searchInput" class="input" type="search" placeholder="세부자격을 검색하세요." v-model="searchQuery"
+            @keyup.enter="handleEnter" />
     </div>
     <div class="table-container">
         <div v-if="isLoading">
@@ -32,7 +33,7 @@
                     <span>응시일 {{ formatDate(item.start_test_date) }}</span>
                 </div>
             </div>
-            <div class="delete_history" >
+            <div class="delete_history" @click="openModal(index)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#03020a" class="bi bi-trash"
                     viewBox="0 0 16 16">
                     <path
@@ -93,6 +94,30 @@
         </li>
     </ul>
     <BottomBar />
+
+    <!-- 삭제 모달 -->
+    <div class="modal" v-if="isShowModal">
+        <div class="card">
+            <div class="header">
+                <div class="image"><svg aria-hidden="true" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                        fill="none">
+                        <path
+                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                            stroke-linejoin="round" stroke-linecap="round"></path>
+                    </svg></div>
+                <div class="content">
+                    <span class="title">기록 삭제</span>
+                    <p class="message">정말로 기록 삭제를 원하십니까? 삭제 시 모든 정보가 삭제되며 복구할 수 없습니다.</p>
+                    <br>
+
+                </div>
+                <div class="actions">
+                    <button class="desactivate" type="button" @click="deleteExamRecord">확인</button>
+                    <button class="cancel" type="button" @click="isShowModal = false">취소</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -113,7 +138,13 @@ export default {
             currentPage: 1, // 현재 페이지
             itemsPerPage: 6, // 페이지당 표시할 항목 수
             totalItems: 0, // 총 항목 수
-            isLoading: false // 로딩 상태
+            isLoading: false, // 로딩 상태
+
+            isShowModal: false,
+            selectedItemIndex: null, // 선택한 항목의 인덱스
+
+            searchQuery: '',
+            filteredItems: [],
         };
     },
     computed: {
@@ -126,24 +157,36 @@ export default {
     },
     methods: {
         async firstPage() {
-            this.currentPage = 1;
-            await this.loadExamRecord();
+            if (this.currentPage > 1) {
+                this.currentPage = 1;
+                await this.loadRecrds();
+            }
         },
         async prevPage() {
             if (this.currentPage > 1) {
                 this.currentPage--;
-                await this.loadExamRecord();
+                await this.loadRecrds();
             }
         },
         async nextPage() {
             if (this.currentPage < Math.ceil(this.totalItems / this.itemsPerPage)) {
                 this.currentPage++;
-                await this.loadExamRecord();
+                await this.loadRecrds();
             }
         },
         async lastPage() {
-            this.currentPage = Math.ceil(this.totalItems / this.itemsPerPage);
-            await this.loadExamRecord();
+            const totalPageCount = Math.ceil(this.totalItems / this.itemsPerPage);
+            if (this.currentPage < totalPageCount) {
+                this.currentPage = totalPageCount;
+                await this.loadRecrds();
+            }
+        },
+        async loadRecrds() {
+            if (this.searchQuery) {
+                await this.searchRecords(); // 검색 중일 때
+            } else {
+                await this.loadExamRecord(); // 검색 중이 아닐 때
+            }
         },
         async loadExamRecord() {
             this.isLoading = true;
@@ -159,6 +202,7 @@ export default {
                 if (response.data.records && Array.isArray(response.data.records)) {
                     this.items = [...response.data.records];
                     this.totalItems = response.data.total;
+                    console.log(this.items)
                 } else {
                     console.error("올바르지 않은 데이터 구조", response.data);
                 }
@@ -176,6 +220,65 @@ export default {
             const hour = date.getHours();
             const minute = date.getMinutes();
             return `${year}. ${month}. ${day} ${hour}시 ${minute}분`;
+        },
+        openModal(index) {
+            this.selectedItemIndex = index;
+            this.isShowModal = true;
+        },
+        async deleteExamRecord() {
+            try {
+                await axios.post("/incorrectNote/deleteExamRecord", {
+                    memberId: this.memberId,
+                    examRecordIdx: this.items[this.selectedItemIndex].exam_record_idx
+                }, {
+                    headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+                });
+                this.items.splice(this.selectedItemIndex, 1);
+                this.isShowModal = false;
+                if (this.items.length == 0) { this.currentPage-- }
+                this.loadExamRecord(); // 새로고침
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async handleEnter() {
+            console.log("enter 키 눌림 현재 입력된 값: ", this.searchQuery);
+            this.$refs.searchInput.blur();
+            await this.searchRecords();
+        },
+
+        async searchRecords() {
+            this.isLoading = true;
+            try {
+                const response = await axios.post("/incorrectNote/searchRecord", {
+                    memberId: this.memberId,
+                    searchQuery: this.searchQuery,
+                    page: this.currentPage,
+                    itemsPerPage: this.itemsPerPage
+                }, {
+                    headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+                });
+
+                if (response.data.records && Array.isArray(response.data.records)) {
+                    this.items = [...response.data.records];
+                    this.totalItems = response.data.total;
+
+                    const totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
+
+                    if (this.currentPage > totalPages) {
+                        this.currentPage = 1;
+                        await this.searchRecords();
+                    } else {
+                        console.log(this.items);
+                    }
+                } else {
+                    console.error("올바르지 않은 데이터 구조", response.data);
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                this.isLoading = false;
+            }
         }
     },
     mounted() {
@@ -348,5 +451,110 @@ strong {
 
 .failed {
     color: #f00;
+}
+
+/* 모달 스타일 */
+.modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.card {
+    overflow: hidden;
+    position: relative;
+    background-color: #ffffff;
+    text-align: left;
+    border-radius: 0.5rem;
+    max-width: 80%;
+    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+}
+
+.header {
+    padding: 1.25rem 1rem 1rem 1rem;
+    background-color: #ffffff;
+}
+
+.image {
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #FEE2E2;
+    flex-shrink: 0;
+    justify-content: center;
+    align-items: center;
+    width: 3rem;
+    height: 3rem;
+    border-radius: 9999px;
+}
+
+.image svg {
+    color: #DC2626;
+    width: 1.5rem;
+    height: 1.5rem;
+}
+
+.content {
+    margin-top: 0.75rem;
+    text-align: center;
+}
+
+.title {
+    color: #111827;
+    font-size: 1rem;
+    font-weight: 600;
+    line-height: 1.5rem;
+}
+
+.message {
+    margin-top: 0.5rem;
+    color: #6B7280;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+}
+
+.actions {
+    margin: 0.75rem 1rem;
+    background-color: #F9FAFB;
+}
+
+.desactivate {
+    display: inline-flex;
+    padding: 0.5rem 1rem;
+    background-color: #DC2626;
+    color: #ffffff;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 500;
+    justify-content: center;
+    width: 100%;
+    border-radius: 0.375rem;
+    border-width: 1px;
+    border-color: transparent;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+}
+
+.cancel {
+    display: inline-flex;
+    margin-top: 0.75rem;
+    padding: 0.5rem 1rem;
+    background-color: #ffffff;
+    color: #374151;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 500;
+    justify-content: center;
+    width: 100%;
+    border-radius: 0.375rem;
+    border: 1px solid #D1D5DB;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
 }
 </style>
