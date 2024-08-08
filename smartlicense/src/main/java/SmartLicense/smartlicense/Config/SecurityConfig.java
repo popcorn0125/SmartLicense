2cf8c990c6d25272a56ae60dce6cb0bedd8608fd
gyk419 <gyk419@gmail.com>
2024-08-08 16:44:20 +0900

    240808 권지용 로컬환경으로 다시 수정

--- smartlicense/src/main/java/SmartLicense/smartlicense/Config/SecurityConfig.java
+++ smartlicense/src/main/java/SmartLicense/smartlicense/Config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)  // csrf 비활성화
-                .cors(withDefaults())
+//                .cors(withDefaults())
                 .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());  // 인증되지 않아도 모든 요청 허용
 
         return http.build();
