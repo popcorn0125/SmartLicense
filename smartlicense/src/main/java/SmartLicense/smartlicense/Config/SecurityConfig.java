70c1918fb20a3371a55beedec92a2fcf8462934b
gyk419 <gyk419@gmail.com>
2024-08-08 14:44:40 +0900

    클라우드타입 배포를위한 cors 수정2

--- smartlicense/src/main/java/SmartLicense/smartlicense/Config/SecurityConfig.java
+++ smartlicense/src/main/java/SmartLicense/smartlicense/Config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
@@ -19,6 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)  // csrf 비활성화
+                .cors(withDefaults())
                 .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());  // 인증되지 않아도 모든 요청 허용
 
         return http.build();
