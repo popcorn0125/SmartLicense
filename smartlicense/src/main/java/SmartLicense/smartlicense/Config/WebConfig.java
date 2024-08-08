a8668cd2810472bc486a529504f0a0e60132928e
김준식 <105399710+popcorn0125@users.noreply.github.com>
2024-08-08 18:19:58 +0900

    240808 김준식 mvcconfig 수정

--- smartlicense/src/main/java/SmartLicense/smartlicense/Config/WebConfig.java
+++ smartlicense/src/main/java/SmartLicense/smartlicense/Config/WebConfig.java
@@ -1,7 +1,9 @@
 package SmartLicense.smartlicense.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,4 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST");
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(new AuthenticationInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/memberLogin/verify","/memberLogin/logout");
+    }
+
+
 }
