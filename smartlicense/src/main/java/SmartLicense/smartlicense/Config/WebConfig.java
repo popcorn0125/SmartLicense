94eee33f1f61d10542a0929c43a33867c9ab1ef9
gyk419 <gyk419@gmail.com>
2024-08-08 12:51:31 +0900

    클라우드타입 배포를위한 cors 설정

--- smartlicense/src/main/java/SmartLicense/smartlicense/Config/WebConfig.java
+++ smartlicense/src/main/java/SmartLicense/smartlicense/Config/WebConfig.java
@@ -15,7 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST");
+                .allowedOrigins("https://web-smartlicense-1272llwyzbyro.sel5.cloudtype.app/")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
